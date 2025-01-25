05508174b6734625274d7ccfcd6e3bef2cf02c13
ZealSV <54489933+ZealSV@users.noreply.github.com>
2025-01-25 12:28:02 -0800

    ai/ml code

---
+++ src/backend/utils/database.py
@@ -0,0 +1,20 @@
+import os
+from pymongo import MongoClient
+
+client = MongoClient(
+    os.getenv("MONGO_URI")
+)
+
+# Pull MongoDB URI from environment variables
+MONGO_URI = os.getenv("MONGO_URI")
+DATABASE_NAME = "test"
+COLLECTION_NAME = "events"
+
+def get_database():
+    client = MongoClient(MONGO_URI)
+    db = client[DATABASE_NAME]
+    return db
+
+def get_events_collection():
+    db = get_database()
+    return db[COLLECTION_NAME]

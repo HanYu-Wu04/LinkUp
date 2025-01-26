c578af644dd86e80312a839a6e4088dde5d4eeb9
HanYu Wu <hanyuwu04@gmail.com>
2025-01-26 08:54:30 -0800

    fixed sidebar layout to ensure other pages can have proper styling

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,10 +3,13 @@ import { Sidebar } from "@/components/Sidebar";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-gray-50">
-      <div className="max-lg:hidden">
+      {/* Sidebar */}
+      <div className="w-64 max-lg:hidden">
         <Sidebar />
       </div>
-      <div className="flex p-8">{children}</div>
+
+      {/* Main Content */}
+      <div className="flex-1">{children}</div>
     </div>
   );
 }
